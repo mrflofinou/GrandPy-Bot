b86abe656727565a80fb7a7f8a64a2b9bfe100a4
Florian <florian.lescuyer@gmail.com>
2018-04-03 11:36:37 +0200

    add tests to test the route and the return string

--- gdpyapp/tests/test_views.py
+++ gdpyapp/tests/test_views.py
@@ -1,3 +1,25 @@
 import pytest
 
+from .. import app
 from .. import views
+from .. import stopwords
+
+
+# Creation of client for the test
+# This client will be use in several tests
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    return client
+
+def test_app_runs(client):
+    res = client.get('/')
+    assert res.status_code == 200
+
+def test_index_print_Hello_World(client):
+    res = client.get('/')
+    assert b"Hello world !" in res.data # 'b' transorm 'str' in 'bytes' type
+
+def test_parser():
+    assert views.parserkiller("") == ""
