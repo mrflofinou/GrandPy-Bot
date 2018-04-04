b425422ea7308848bc16f48d7d5a56c416308337
Florian <florian.lescuyer@gmail.com>
2018-04-04 17:39:06 +0200

    add tests for connexion at mediawiki and for the return of the API

--- gdpyapp/tests/test_views.py
+++ gdpyapp/tests/test_views.py
@@ -2,14 +2,12 @@ import pytest
 
 from .. import app
 from .. import views
-from .. import stopwords
-
 
 # Creation of client for the test
 # This client will be use in several tests
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config.from_object("gdpyapp.tests.config")
     client = app.test_client()
     return client
 
@@ -17,9 +15,13 @@ def test_app_runs(client):
     res = client.get('/')
     assert res.status_code == 200
 
-def test_index_print_Hello_World(client):
+def test_index_print_hello_world(client):
     res = client.get('/')
     assert b"Hello world !" in res.data # 'b' transorm 'str' in 'bytes' type
 
-def test_parser():
-    assert views.parserkiller("") == ""
+def test_connexion_mediawiki_API():
+    assert views.get_from_mediawiki_API("https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2").status_code == 200
+
+def test_results_return_text(client):
+    res = client.get('/results/?text=le-louvre-paris')
+    assert "Le musée du Louvre, inauguré en 1793 sous l'appellation Muséum central des arts de la République dans le palais du Louvre, ancienne résidence royale située au centre de Paris, est aujourd'hui le plus grand musée d'art et d'antiquités au monde" in str(res.data)
