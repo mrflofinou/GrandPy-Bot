e796acab91a6b3208341ad30f55b45da45e585c9
Florian <florian.lescuyer@gmail.com>
2018-04-04 17:37:59 +0200

    add route for results page and function to connect at mediawiki

--- gdpyapp/views.py
+++ gdpyapp/views.py
@@ -1,7 +1,20 @@
-from flask import Flask
+import requests
+from flask import Flask, request
+
 
 app = Flask(__name__)
+
+def get_from_mediawiki_API(url):
+    """ Connexion to the wikipedia mediawiki API """
+    return requests.get(url)
 
 @app.route('/')
 def index():
     return "Hello world !"
+
+# /results?text=livre-truc-a
+@app.route('/results/')
+def form_result():
+    return request.args.get("text")
+
+# https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2
