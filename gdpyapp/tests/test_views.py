909df0addc0953586ac94017833cfab14c6ee4fd
Florian <florian.lescuyer@gmail.com>
2018-04-12 17:59:07 +0200

    Modification of test_results_runs and test_results_return_text for http post method

--- gdpyapp/tests/test_views.py
+++ gdpyapp/tests/test_views.py
@@ -6,37 +6,6 @@ from selenium import webdriver
 from .. import app
 from .. import views
 
-# Creation of client for the test
-# This client will be use in several tests
-# @pytest.fixture
-# def client():
-#     app.config.from_object("gdpyapp.tests.config")
-#     client = app.test_client()
-#     return client
-#
-# @pytest.fixture
-# def selenium():
-#     driver = webdriver.Firefox()
-#     yield driver
-#     driver.quit()
-#
-# def test_app_runs(client):
-#     res = client.get('/')
-#     assert res.status_code == 200
-#
-# def test_index_print_hello_world(client):
-#     res = client.get('/')
-#     assert b"Hello world !" in res.data # 'b' transorm 'str' in 'bytes' type
-#
-# def test_connexion_mediawiki_API():
-#     assert client.get('/results/) == 200
-
-# def test_results_return_text(client):
-#     res = client.get('/results/?text=le-louvre-paris')
-#     assert "le-louvre-paris" in str(res.data)
-
-
-
 class gdpyapp_test(LiveServerTestCase):
     def create_app(self):
         app.config.from_object('gdpyapp.tests.config')
@@ -56,12 +25,14 @@ class gdpyapp_test(LiveServerTestCase):
 
     def test_index_print_hello_world(self):
         res = requests.get(self.get_server_url())
-        assert "Hello world !" in res.text
+        assert "Grand Py Bot" in res.text
 
-    def test_connexion_mediawiki_API(self):
-        res = requests.get(self.get_server_url() + '/results/')
+    def test_results_runs(self):
+        payload = {'request': 'musée du louvre'}
+        res = requests.post(self.get_server_url() + '/results/', data=payload)
         assert res.status_code == 200
 
     def test_results_return_text(self):
-        res = requests.get(self.get_server_url() + '/results/')
-        assert "Le musée du Louvre, inauguré en 1793 sous l'appellation Muséum central des arts de la République dans le palais du Louvre, ancienne résidence royale située au centre de Paris, est aujourd'hui le plus grand musée d'art et d'antiquités au monde" in res.text
+        payload = {'request': 'musée du louvre'}
+        res = requests.post(self.get_server_url() + '/results/', data=payload)
+        assert "Le <b>musée du Louvre</b>, inauguré en 1793 sous l'appellation <i>Muséum central des arts de la République</i> dans le palais du Louvre, ancienne résidence royale située au centre de Paris, est aujourd'hui le plus grand musée d'art et d'antiquités au monde." in res.text
