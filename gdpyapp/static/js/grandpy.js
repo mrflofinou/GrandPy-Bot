88e5127879b07452b64ce0d2a3c2fa4a1180e4f5
Florian <florian.lescuyer@gmail.com>
2018-06-01 17:49:46 +0200

    Add error message when don't have map

--- gdpyapp/static/js/grandpy.js
+++ gdpyapp/static/js/grandpy.js
@@ -7,19 +7,31 @@ form.addEventListener("submit", function(e) {
     var data = new FormData(form);
     // AJAX post to Flask view "/index/"
     ajaxPost("http://localhost:5000/results/", data, function(response) {
+        document.getElementById("presentation").style.display = 'none';
         // I convert the JSON from Flask in javascript object
+        // This Json contain the text from wikipedia and the place_id from gogle maps
         response = JSON.parse(response)
         var pElt = document.getElementById("text");
         pElt.innerHTML = "";
-        // The text
+        // The text from the Flask
         pElt.textContent = response.wikipedia;
         document.getElementById("wikipedia").style.display = 'block';
 
-        var urlGoogleMapsApi = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAOcEnqP9jbC1eApGVZ6iYHtD4jNdXrFco&q=place_id:";
+        var urlGoogleMapsApi = "https://www.google.com/maps/embed/v1/place";
         // I use the place_id from Flask JSON
-        var googleMapsRequest = urlGoogleMapsApi + response.maps;
-        var iframeElt = document.getElementById("map");
-        iframeElt.src = googleMapsRequest;
-        document.getElementById("googlemaps").style.display = 'block';
+        if (response.place_id !== "null") {
+            var googleMapsRequest = urlGoogleMapsApi + "?key=" + response.google_key + "&q=place_id:" + response.place_id;
+            var iframeElt = document.getElementById("map");
+            iframeElt.src = googleMapsRequest;
+            document.getElementById("googlemaps").style.display = 'block';
+            document.getElementById("map").style.display = 'block';
+            document.getElementById("mapError").style.display = 'none';
+        } else {
+            document.getElementById("googlemaps").style.display = 'block';
+            document.getElementById("mapError").style.display = 'block';
+            document.getElementById("map").style.display = 'none';
+            var pElt = document.getElementById("mapError");
+            pElt.textContent = "Oups ! Je n'arrive pas à mettre le doigt sur l'adresse. Peux-tu être un peu plus précis ? Par exemple tu pourrais m'indiquer la ville où se trouve ce que tu cherches.";
+        }
     });
 });
