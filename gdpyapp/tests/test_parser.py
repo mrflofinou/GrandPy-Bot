1537f0567a4b6ba5072e349615382f927ed2e699
Florian <florian.lescuyer@gmail.com>
2018-06-01 17:46:36 +0200

    Modification to return strings

--- gdpyapp/tests/test_parser.py
+++ gdpyapp/tests/test_parser.py
@@ -3,14 +3,17 @@ import pytest
 from ..helpers import parser, stopwords
 
 
-def test_parser_return_list():
-    assert type(parser.parserkiller(' '.join(stopwords.stopword))) == list
+def test_parser_return_string():
+    assert type(parser.parserkiller(' '.join(stopwords.stopword))) == str
 
 def test_parser_return_void_when_only_stop_words():
-    assert parser.parserkiller(' '.join(stopwords.stopword)) == []
+    assert parser.parserkiller(' '.join(stopwords.stopword)) == ""
 
 def test_parser_return_input_when_no_stop_words():
-    assert parser.parserkiller("maison café turban") == ["maison", "café", "turban"]
+    assert parser.parserkiller("maison café turban") == "maison café turban"
 
 def test_parser_return_input_without_stop_words():
-    assert parser.parserkiller("La maison de mon grand-père est toute petite") == ["maison", "grand-père", "petite"]
+    assert parser.parserkiller("La maison de mon grand-père est toute petite") == "maison grand-père petite"
+
+def test_parser_return_void_when_only_punctuation():
+    assert parser.parserkiller("? !") == ""
