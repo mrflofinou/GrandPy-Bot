6c82d315dd3ab7d1e22fb114cebf0e64c1d64467
Florian <florian.lescuyer@gmail.com>
2018-06-01 17:48:17 +0200

    Add test of exception cases

--- gdpyapp/tests/test_views.py
+++ gdpyapp/tests/test_views.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 
 from .. import app
 from .. import views
+from ..helpers import gdpy_sentences
 
 class gdpyapp_test(LiveServerTestCase):
     def create_app(self):
@@ -23,10 +24,6 @@ class gdpyapp_test(LiveServerTestCase):
         res = requests.get(self.get_server_url())
         assert res.status_code == 200
 
-    def test_index_print_grand_py_bot(self):
-        res = requests.get(self.get_server_url())
-        assert "Grand Py Bot" in res.text
-
     def test_result_runs(self):
         payload = {'request': 'musée du louvre'}
         res = requests.post(self.get_server_url() + "/results/", data=payload)
@@ -35,9 +32,42 @@ class gdpyapp_test(LiveServerTestCase):
     def test_results_return_text(self):
         payload = {'request': 'musée du louvre'}
         res = requests.post(self.get_server_url() + "/results/", data=payload)
-        assert "Le musée du Louvre, inauguré en 1793 sous l'appellation Muséum central des arts de la République dans le palais du Louvre, ancienne résidence royale située au centre de Paris, est aujourd'hui le plus grand musée d'art et d'antiquités au monde." in res.text
+        res = res.json()
+        assert "Le musée du Louvre, inauguré en 1793 sous l'appellation Muséum central des arts de la République dans le palais du Louvre, ancienne résidence royale située au centre de Paris, est aujourd'hui le plus grand musée d'art et d'antiquités au monde." in res["wikipedia"]
+
+    def test_results_return_place_id(self):
+        payload = {'request': 'musée du louvre'}
+        res = requests.post(self.get_server_url() + "/results/", data=payload)
+        res = res.json()
+        assert "ChIJD3uTd9hx5kcR1IQvGfr8dbk" in res["place_id"]
 
     def test_results_return_error_message_when_form_is_void(self):
         payload = {'request': ''}
         res = requests.post(self.get_server_url() + "/results/", data=payload)
-        assert "Me demander quelque chose tu dois" in res.text
+        res = res.json()
+        assert gdpy_sentences.gdpy_need_informations in res["wikipedia"]
+
+    def test_results_return_place_id_null_when_form_is_void(self):
+        payload = {'request': ''}
+        res = requests.post(self.get_server_url() + "/results/", data=payload)
+        res = res.json()
+        assert "null" in res["place_id"]
+
+    def test_results_return_error_message_when_form_is_random_letters(self):
+        payload = {'request': 'frsdlkvz'}
+        res = requests.post(self.get_server_url() + "/results/", data=payload)
+        res = res.json()
+        assert gdpy_sentences.gdpy_dont_understand in res["wikipedia"]
+
+    def test_results_return_place_id_null_when_form_is_random_letters(self):
+        payload = {'request': 'frsdlkvz'}
+        res = requests.post(self.get_server_url() + "/results/", data=payload)
+        res = res.json()
+        assert "null" in res["place_id"]
+
+    # def test_home_display(self):
+    #     # Le navigateur est Firefox
+    #     self.driver = webdriver.Firefox()
+    #     self.driver.get(self.get_server_url())
+    #     assert self.driver.current_url == 'http://localhost:8943/'
+    #     self.driver.quit()
