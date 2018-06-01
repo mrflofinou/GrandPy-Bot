b89892b4f8a8c5f3114fe83087228b5f8dfc11dc
Florian <florian.lescuyer@gmail.com>
2018-06-01 17:47:26 +0200

    Modification to return google_key

--- gdpyapp/views.py
+++ gdpyapp/views.py
@@ -1,16 +1,17 @@
 from flask import Flask, request, render_template, jsonify
 
-from .helpers import parser, mediawiki, maps
+from .helpers import API_helper, parser
 
 
 app = Flask(__name__)
+
+app.config.from_object('config')
 
 @app.route('/')
 @app.route('/index/')
 def index():
     return render_template('index.html')
 
-# @app.route('/',  methods=['GET', 'POST'])
 @app.route('/results/', methods=['GET', 'POST'])
 def results():
     # To handle the request from the form
@@ -18,8 +19,8 @@ def results():
     # To use a parser with user request from the form
     form_parsing = parser.parserkiller(str(form_result))
     # Make a search from the parser's words with mediawiki API
-    response = mediawiki.wikipedia(form_parsing)
+    response = API_helper.ApiHelper.get_wikipedia_result(form_parsing)
     # google maps search
-    place_id = maps.maps(form_parsing)
+    place_id = API_helper.ApiHelper.get_google_map(form_parsing, app.config["GOOGLE_KEY"])
     # I create a response in JSON. I will use it with the javascript file
-    return jsonify({"wikipedia":response, "maps":place_id})
+    return jsonify({"wikipedia":response, "place_id":place_id, "google_key":app.config["GOOGLE_KEY"]})
