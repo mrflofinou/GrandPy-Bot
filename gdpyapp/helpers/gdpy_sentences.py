80788e33dfd4ef5c2af18b4606a10750b48a83f1
Florian <florian.lescuyer@gmail.com>
2018-06-01 17:34:33 +0200

    contains sentences of GrandPy Bot

---
+++ gdpyapp/helpers/gdpy_sentences.py
@@ -0,0 +1,6 @@
+"""
+This file group the sentences of GrandPy Bot
+"""
+
+gdpy_dont_understand = "Oups ! Tu as du faire une erreur mon poussinet. Je ne comprend pas ce que tu me demandes."
+gdpy_need_informations = "J'ai besoin de plus d'inforations mon poussin."
