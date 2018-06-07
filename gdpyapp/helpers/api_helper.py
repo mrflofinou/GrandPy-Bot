3deb4d178c048527b6e2efe9c441a2d2c816c039
Florian <florian.lescuyer@gmail.com>
2018-06-07 16:50:35 +0200

    rename and add exceptions

---
+++ gdpyapp/helpers/api_helper.py
@@ -0,0 +1,103 @@
+import requests
+from bs4 import BeautifulSoup
+
+from .exceptions import GoogleError, QueryEmptyError, WikipediaResultError
+from .gdpy_sentences import gdpy_need_informations, gdpy_dont_understand
+
+
+class ApiHelper:
+    """
+    This class group methods to connect to the APIs:
+        - Google Mpas to get informations for a location
+        - Wikimedia to get informations from wikipedia
+    """
+
+    WIKIMEDIA_API_URL = "https://fr.wikipedia.org/w/api.php?action=query&rvprop=content&format=json&formatversion=2"
+
+    @classmethod
+    def get_google_map_place_id(cls, query, google_key):
+        """
+        This method allow to communicate with google maps API
+        The goal of this method is to get the id of a place (place_id)
+        to display it on a map
+        """
+
+        search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+        search_params = {"query":query, "key":google_key}
+        # I make a request at google maps API to search a location
+        search_request = requests.get(search_url, params=search_params)
+        search_json = search_request.json()
+        # If Google Maps don't find results
+        if search_json["status"] != "OK":
+            raise GoogleError
+        else:
+            # I get the id of the first place of the list of the search results
+            place_id = search_json["results"][0]["place_id"]
+        # The place_id will use in the iframe of google maps to display the location
+        return place_id
+
+    @classmethod
+    def get_google_map_adress(cls, place_id, google_key):
+        """
+        This method allow to communicate with google maps API
+        The goal of this method is to get the id of a place (place_id)
+        to display it on a map
+        """
+
+        search_url = "https://maps.googleapis.com/maps/api/place/details/json"
+        search_params = {"placeid":place_id, "key":google_key}
+        # I make a request at google maps API to search a location
+        search_request = requests.get(search_url, params=search_params)
+        search_json = search_request.json()
+        # If Google Maps don't find results
+        if search_json["status"] != "OK":
+            raise GoogleError
+        else:
+            # I get the adress of the place
+            adress = search_json["result"]["formatted_address"]
+        return adress
+
+    @classmethod
+    def _get_wikipedia_page_id(cls, query):
+        """
+        This method communicate with the wikimedia API to get the wikipedia page id of the query.
+        This id will be use to find the page for the query.
+        """
+
+        if len(query) == 0:
+            raise QueryEmptyError
+        else:
+            search_params = {"list": "search", "prop": "revisions","srsearch":query}
+            search = requests.get(cls.WIKIMEDIA_API_URL, params=search_params)
+            search_data = search.json()
+            search_results = search_data["query"]["search"]
+            if len(search_results) == 0:
+                raise WikipediaResultError
+            else:
+                # We consider the first result of the search is the most relevant
+                page_id = search_results[0]["pageid"]
+
+        return page_id
+
+    @classmethod
+    def get_wikipedia_result(cls, query):
+        """
+        This method allow to communicate with wikimedia API
+        The goal of this methos is to get the first lines of the dercription
+        of a place.
+        We use the id of a page to find it and get the first paragraph
+        """
+
+        page_id = cls._get_wikipedia_page_id(query)
+        search_params = {"prop": "extracts" , "pageids": page_id}
+        # Make a request to obtain the page of the previous first result with its page id
+        page_request = requests.get(cls.WIKIMEDIA_API_URL, params=search_params)
+        page_data = page_request.json()
+        # To scrap the result
+        wikipedia_html_doc = page_data["query"]["pages"][0]["extract"]
+        # BeautifulSoup is a HTML scraper
+        soup = BeautifulSoup(wikipedia_html_doc, "html.parser")
+        # return the first paragraph of the page
+        response = soup.find('p').text
+
+        return response
