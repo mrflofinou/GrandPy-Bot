b13d30cfe2262ce5412559aa7647214ecaa4c35b
Florian <florian.lescuyer@gmail.com>
2018-06-07 16:52:20 +0200

    add new sentences for Grand Py

--- gdpyapp/helpers/gdpy_sentences.py
+++ gdpyapp/helpers/gdpy_sentences.py
@@ -1,6 +1,8 @@
 """
 This file group the sentences of GrandPy Bot
 """
-
-gdpy_dont_understand = "Oups ! Tu as du faire une erreur mon poussinet. Je ne comprend pas ce que tu me demandes."
-gdpy_need_informations = "J'ai besoin de plus d'inforations mon poussin."
+gdpy_adress = ["Ce lieu m'évoque tellement de souvenirs ! En voici l'adresse mon poussin :", "J'adore cet endroit ! Voici l'adresse pour que tu puisse t'y rendre aussi :", "Voila ça vient. Ne soit pas trop pressé, je n'ai plus 20 ans !"]
+gdpy_story = ["Comme tu es gentil, je vais étaler un peu de ma culture rien que pour toi. Je suis sûr que ce qui va suivre va grandement t'intéresser.", "J'ai très envie de te raconter un petit quelque chose sur cet endroit.", "J'adore raconter l'histoire de ce lieu. Je pourrais en parler des heures !"]
+gdpy_dont_understand = "Sacrebleu ! Tu as du faire une erreur mon poussinet. Si tu pouvais éviter ton langage de jeune ça m'arrangerait. J'ai quand même des limites !"
+gdpy_need_informations = "Mais de qui se moque-t-on ? J'ai besoin de plus d'informations mon poussin !"
+gdpy_dont_find_adress = "Saperlipopette ! Je ne trouve pas l'adresse du lieu que tu cherches. Tu sais à mon âge, on commence à perdre la boule. Sûrement que si tu me donnais plus de précisions, comme une ville, ça me rafraichirait la mémoire."
