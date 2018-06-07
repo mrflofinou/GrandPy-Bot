b869bc8e8e5bd6482428f04558f03cd1a248a9f7
Florian <florian.lescuyer@gmail.com>
2018-06-07 16:53:15 +0200

    modification to delete '-' in query

--- gdpyapp/helpers/parser.py
+++ gdpyapp/helpers/parser.py
@@ -6,6 +6,7 @@ def parserkiller(query):
     This function parse a sentence with the help of stop words
     The result will be use for the APIs
     """
-    query_splited = query.lower().split()
+    query_modified = query.replace("-", " ")
+    query_splited = query_modified.lower().split()
     result_parser = [word for word in query_splited if word not in stopword]
     return ' '.join(result_parser)
