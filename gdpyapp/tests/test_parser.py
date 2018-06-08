4f323cf1ea1060440f9adc029b0f9a72ccbf3850
Florian <florian.lescuyer@gmail.com>
2018-06-08 10:21:12 +0200

    Last modifications

--- gdpyapp/tests/test_parser.py
+++ gdpyapp/tests/test_parser.py
@@ -10,10 +10,10 @@ def test_parser_return_void_when_only_stop_words():
     assert parser.parserkiller(' '.join(stopwords.stopword)) == ""
 
 def test_parser_return_input_when_no_stop_words():
-    assert parser.parserkiller("maison café turban") == "maison café turban"
+    assert parser.parserkiller("maison café-turban") == "maison café turban"
 
 def test_parser_return_input_without_stop_words():
-    assert parser.parserkiller("La maison de mon grand-père est toute petite") == "maison grand-père petite"
+    assert parser.parserkiller("La maison de mon oncle est toute petite") == "maison oncle petite"
 
 def test_parser_return_void_when_only_punctuation():
-    assert parser.parserkiller("? !") == ""
+    assert parser.parserkiller("? ! , ' ; :") == ""
